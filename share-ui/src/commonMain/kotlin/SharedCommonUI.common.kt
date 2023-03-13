61de0538b318803a0bcfb5bc1854cff35875ca0f
dima.avdeev <dima.avdeev@jetbrains.com>
2023-03-13 21:05:16 +0400

    copy share-us

---
+++ share-ui/src/commonMain/kotlin/SharedCommonUI.common.kt
@@ -0,0 +1,13 @@
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.random.Random
+
+@Composable
+internal fun SharedCommonUI() {
+    Text("SharedCommonUI")
+}
