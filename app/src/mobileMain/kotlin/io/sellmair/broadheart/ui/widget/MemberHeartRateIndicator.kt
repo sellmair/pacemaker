7d81d7ca9321b905b69fca705e90ae7a885d338f
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-03-15 18:23:36 +0100

    MemberHeartRateIndicator: Use rememberCoroutineScope to launch animation

--- app/src/mobileMain/kotlin/io/sellmair/broadheart/ui/widget/MemberHeartRateIndicator.kt
+++ app/src/mobileMain/kotlin/io/sellmair/broadheart/ui/widget/MemberHeartRateIndicator.kt
@@ -9,24 +9,25 @@ import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import io.sellmair.broadheart.*
+import io.sellmair.broadheart.displayColorLight
 import io.sellmair.broadheart.model.HeartRate
 import io.sellmair.broadheart.service.GroupMemberState
 import io.sellmair.broadheart.ui.toColor
+import kotlinx.coroutines.launch
 
 @Composable
 internal fun MemberHeartRateIndicator(state: GroupMemberState, range: ClosedRange<HeartRate>) {
     val side = if (state.user?.isMe == true) ScaleSide.Right else ScaleSide.Left
     if (state.currentHeartRate == null) return
 
-    val animatedHeartRate = remember(state.user) { Animatable(state.currentHeartRate.value) }
-    LaunchedEffect(state.user) {
+    val animatedHeartRate = remember("animate", state.user?.id) { Animatable(state.currentHeartRate.value) }
+    rememberCoroutineScope().launch {
         animatedHeartRate.animateTo(state.currentHeartRate.value)
     }
 
