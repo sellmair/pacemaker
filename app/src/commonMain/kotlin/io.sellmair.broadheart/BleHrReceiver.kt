ccbf3dc7cf656d264124737e2f7939438db61288
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-03-24 10:34:13 +0100

    Move Ble based HR receiver to common

--- app/src/commonMain/kotlin/io.sellmair.broadheart/BleHrReceiver.kt
+++ app/src/commonMain/kotlin/io.sellmair.broadheart/BleHrReceiver.kt
@@ -1,4 +1,5 @@
-import io.sellmair.broadheart.HeartRateReceiver
+package io.sellmair.broadheart
+
 import io.sellmair.broadheart.bluetooth.Ble
 import io.sellmair.broadheart.bluetooth.receiveHeartRateMeasurements
 import io.sellmair.broadheart.model.HeartRateMeasurement
@@ -6,7 +7,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.flow
 
-fun AndroidBleHeartRateReceiver(ble: Ble): HeartRateReceiver {
+fun BleHeartRateReceiver(ble: Ble): HeartRateReceiver {
     return object : HeartRateReceiver {
         override val measurements: Flow<HeartRateMeasurement>
             get() = flow { emitAll(ble.receiveHeartRateMeasurements()) }
