4b0cdf41fac241de640b0e603fa6ba900a20f538
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-06-10 23:27:40 +0200

    Simple MeshBackdrop for iOS

--- app/src/appleMain/kotlin/MeshBackdrop.kt
+++ app/src/appleMain/kotlin/MeshBackdrop.kt
@@ -2,8 +2,22 @@
 
 package io.sellmair.pacemaker.ui.settingsPage
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 
 @Composable
-internal actual fun MeshBackdrop(modifier: Modifier) = Unit
+internal actual fun MeshBackdrop(modifier: Modifier) {
+    Box(
+        modifier = modifier.background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(Color(0xFF667EEA), Color(0xFF64B6FF)),
+                    startY = 0f,
+                    endY = 500f
+                )
+            )
+    )
+}
