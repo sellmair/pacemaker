aa5905e590d9046f786a9f2497b35c2fc9a75ab2
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-11-17 16:31:45 +0100

    Fix iOS compilation and 'StateBusTest'

--- app/src/iosMain/kotlin/IosPacemakerViewController.kt
+++ app/src/iosMain/kotlin/IosPacemakerViewController.kt
@@ -3,12 +3,8 @@ package io.sellmair.pacemaker.ui
 import IosApplicationBackend
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.window.ComposeUIViewController
-import io.sellmair.pacemaker.ApplicationViewModel
-import kotlinx.coroutines.MainScope
 
 private val backend by lazy { IosApplicationBackend() }
-
-private val viewModel by lazy { ApplicationViewModel(MainScope(), backend) }
 
 @Suppress("Unused") // Entry point for iOS application!
 object IosPacemakerViewController {
@@ -18,7 +14,7 @@ object IosPacemakerViewController {
             LocalStateBus provides backend.stateBus,
             LocalSessionService provides backend.sessionService
         ) {
-            ApplicationWindow(viewModel)
+            ApplicationWindow()
         }
     }
 }
