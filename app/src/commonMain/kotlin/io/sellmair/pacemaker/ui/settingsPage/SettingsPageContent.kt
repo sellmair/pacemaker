d936edaf15dbd35d91065809e8d04a6c5fc149c2
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-11-24 17:48:22 +0100

    Fix small glitch when opening Settings Page

--- app/src/commonMain/kotlin/io/sellmair/pacemaker/ui/settingsPage/SettingsPageContent.kt
+++ app/src/commonMain/kotlin/io/sellmair/pacemaker/ui/settingsPage/SettingsPageContent.kt
@@ -2,7 +2,16 @@
 package io.sellmair.pacemaker.ui.settingsPage
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
@@ -57,7 +66,10 @@ internal fun SettingsPageDevicesList(
         Spacer(modifier = Modifier.height(24.dp))
 
 
-        LazyColumn(horizontalAlignment = Alignment.CenterHorizontally) {
+        LazyColumn(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth()
+        ) {
 
             if (heartRateSensors.isEmpty()) {
                 item(key = "empty placeholder") {
