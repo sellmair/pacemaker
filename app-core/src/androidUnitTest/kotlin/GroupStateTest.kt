812255d109266a2214e9ef5158a80293373ad381
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2024-03-17 13:17:38 +0100

    GroupStateTest 🫠

--- app-core/src/androidUnitTest/kotlin/GroupStateTest.kt
+++ app-core/src/androidUnitTest/kotlin/GroupStateTest.kt
@@ -9,8 +9,10 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.*
 import kotlinx.datetime.Clock
 import utils.createInMemoryDatabase
+import kotlin.coroutines.CoroutineContext
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.fail
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class GroupStateTest {
@@ -20,61 +22,56 @@ class GroupStateTest {
     @Test
     fun `sample 0`() = test {
         val meSensorId = HeartRateSensorId("me")
-        userService.linkSensor(userService.me(), meSensorId)
+        val me = userService.me()
+        userService.linkSensor(me, meSensorId)
         HeartRateMeasurementEvent(HeartRate(128f), meSensorId, Clock.System.now()).emit()
 
-        launch(StandardTestDispatcher(testScheduler), start = CoroutineStart.UNDISPATCHED) {
+        launch {
             GroupState.get().collect { println("(${testScheduler.currentTime}ms) s: $it") }
         }
 
-        withContext(UnconfinedTestDispatcher(testScheduler)) {
-            testScheduler.runCurrent()
 
-            val state = GroupState.get().first { it.members.isNotEmpty() }
-            println("(${testScheduler.currentTime}ms) Received first state with non-empty members")
+        val state = GroupState.get().first { it.members.isNotEmpty() }
+        println("(${testScheduler.currentTime}ms) Received first state with non-empty members")
 
-            assertEquals(
-                GroupState(
-                    listOf(
-                        UserState(
-                            user = userService.me(),
-                            isMe = true,
-                            heartRate = HeartRate(128f),
-                            heartRateLimit = UserService.NewUserHeartRateLimit.value(),
-                            color = UserColors.default(userService.me().id)
-                        )
+        assertEquals(
+            GroupState(
+                listOf(
+                    UserState(
+                        user = me,
+                        isMe = true,
+                        heartRate = HeartRate(128f),
+                        heartRateLimit = UserService.NewUserHeartRateLimit.value(),
+                        color = UserColors.default(me.id)
                     )
-                ), state,
-                "Expect first state with non-empty members to reflect the previously emitted '${HeartRateMeasurementEvent::class}'"
-            )
+                )
+            ), state,
+            "Expect first state with non-empty members to reflect the previously emitted '${HeartRateMeasurementEvent::class}'"
+        )
 
-            println("(${testScheduler.currentTime}ms) Testing current state value...")
-            assertEquals(
-                state, GroupState.get().value,
-                "(${testScheduler.currentTime}ms) Expect the current value of 'GroupState' to be the recently emitted state"
-            )
+        println("(${testScheduler.currentTime}ms) Testing current state value...")
+        assertEquals(
+            state, GroupState.get().value,
+            "(${testScheduler.currentTime}ms) Expect the current value of 'GroupState' to be the recently emitted state"
+        )
 
-            println("(${testScheduler.currentTime}ms) Waiting until the Group state resets..")
-            GroupState.get().first { it == GroupState.default }
+        println("(${testScheduler.currentTime}ms) Waiting until the Group state resets..")
+        GroupState.get().first { it == GroupState.default }
 
-            println("(${testScheduler.currentTime}ms) Group state was reset!")
-            assertEquals(
-                GroupState.KeepMeasurementDuration.value().inWholeMilliseconds,
-                testScheduler.currentTime,
-                "Expect that the default value was emitted after the correct time"
-            )
-        }
+        println("(${testScheduler.currentTime}ms) Group state was reset!")
+        assertEquals(
+            GroupState.KeepMeasurementDuration.value().inWholeMilliseconds,
+            testScheduler.currentTime,
+            "Expect that the default value was emitted after the correct time"
+        )
     }
 
-
-    @OptIn(ExperimentalStdlibApi::class, ExperimentalCoroutinesApi::class)
     private fun test(block: suspend TestScope.() -> Unit) = runTest(EventBus() + StateBus()) {
         try {
-            launchGroupStateActor(userService, actorContext = StandardTestDispatcher(testScheduler))
+            launchGroupStateActor(userService, actorContext = currentCoroutineContext())
             block()
         } finally {
             currentCoroutineContext().cancelChildren()
-            Dispatchers.resetMain()
         }
     }
 }
